f6e95b3d6ea757ba45430ff5962f07270e9d6a27
Thiago Reis <reist36@gmail.com>
2023-09-20 17:50:45 -0300

    feat: creating filters box

--- ryustore/src/app/page.tsx
+++ ryustore/src/app/page.tsx
@@ -1,10 +1,11 @@
 import Image from 'next/image'
 import styles from './page.module.css'
+import { FilterBar } from '@/components/filter-bar'
 
 export default function Home() {
   return (
     <main className={styles.main}>
-      <h1>Olá mundo</h1>
+      <FilterBar></FilterBar>
     </main>
   )
 }
